ed5cd346e43065123b826de8e6b5f49ef980ae34
Jesse Quinn <me@jessequinn.info>
2019-02-06 17:51:16 -0200

    added manage.py and requirements.txt

--- frontend_docker/config.py
+++ frontend_docker/config.py
@@ -5,7 +5,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '\xd9w\xb5\xa1\x13\x82\xadC\xa2J\x81*e\xc6\x1c8\x8b\x92S3\r\x7fAl'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
