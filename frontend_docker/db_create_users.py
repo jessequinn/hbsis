4a96edd4e259cf834ca9b65d3eb0a2f5ebf774e3
Jesse Quinn <me@jessequinn.info>
2019-02-07 13:39:28 -0200

    updated gitignore

--- frontend_docker/db_create_users.py
+++
@@ -1,9 +0,0 @@
-from project import db
-from project.models import User
-
-# insert data
-db.session.add(User('test','test'))
-db.session.add(User('admin','admin'))
-
-# commit the changes
-db.session.commit()
